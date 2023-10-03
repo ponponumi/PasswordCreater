dbb2602287fe74fad5b0a1fc987e1e26145d674d
ponponumi <ponponumi@gmail.com>
2023-10-03 23:05:01 +0900

    バージョンを変更

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,10 @@ class PasswordCreater{
 
   private upperGet() {
     // 大文字を取得する
+    let lower: string;
     let upper: string;
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    lower = this.lowerGet();
+    upper = lower.toUpperCase();
 
     return upper;
   }
@@ -41,11 +43,20 @@ class PasswordCreater{
     if (this.notSymbolArray) {
       this.notSymbolArray.forEach(notSymbol => {
         // 使わない記号を消していく
-        symbol = symbol.replace(new RegExp(notSymbol, "L"), "");
+        symbol = symbol.replace(notSymbol, "");
       });
     }
 
     return symbol;
+  }
+
+  private random(max: number) {
+    // 乱数を生成する
+    let arr = new Uint32Array(1);
+    crypto.getRandomValues(arr);
+    let result: number = arr[0] % max;
+
+    return result;
   }
 
   private stringShuffle(string: string) {
@@ -53,7 +64,7 @@ class PasswordCreater{
     let array: string[] = string.split("");
 
     for (let i: number = array.length - 1; i > 0; i--) {
-      let j: number = Math.floor(Math.random() * (i + 1));
+      let j: number = this.random(array.length);
       [array[i], array[j]] = [array[j], array[i]];
     }
 
@@ -62,20 +73,18 @@ class PasswordCreater{
 
   private createCore(text: string) {
     // パスワードの生成のコア部分
-    let result = "";
-    let last = "";
+    let result: string = "";
+    let last: string = "";
 
     // テキストをリストにした上で、シャッフルする
     let stringList: string[] = this.stringShuffle(text);
 
     while (result.length < this.length) {
       // 長さを満たすまでループ
-      let arr = new Uint32Array(1);
-      crypto.getRandomValues(arr);
-      let index = arr[0] % stringList.length;
-      let add = stringList[index];
+      let index: number = this.random(stringList.length);
+      let add: string = stringList[index];
 
-      if (add != last) {
+      if (add.toLowerCase() != last.toLowerCase()) {
         // 文字が連続しなければ
         result += add;
         last = add;
@@ -90,13 +99,16 @@ class PasswordCreater{
     let text: string = this.lowerGet() + this.upperGet() + this.numberGet() + this.symbolGet();
     let array: string[] = this.stringShuffle(text);
     console.log(array);
+    console.log(text);
+    console.log(text.length);
   }
+
+  // ここから下は他アプリからの呼び出し用のメソッド
 
   public createAll() {
     // 全ての文字を使ってパスワードを生成
     let text: string = this.lowerGet() + this.upperGet() + this.numberGet() + this.symbolGet();
     let result: string = this.createCore(text);
-    console.log(result);
     return result;
   }
 
@@ -104,7 +116,6 @@ class PasswordCreater{
     // 英数字を使ってパスワード生成
     let text: string = this.lowerGet() + this.upperGet() + this.numberGet();
     let result: string = this.createCore(text);
-    console.log(result);
     return result;
   }
 
@@ -112,7 +123,41 @@ class PasswordCreater{
     // 数字のみのパスワードを生成(原則非推奨)
     let text: string = this.numberGet();
     let result: string = this.createCore(text);
-    console.log(result);
+    return result;
+  }
+
+  public createUpperNumber() {
+    // 大文字と数字
+    let text: string = this.upperGet() + this.numberGet();
+    let result: string = this.createCore(text);
+    return result;
+  }
+
+  public createLowerNumber() {
+    // 小文字と数字
+    let text: string = this.lowerGet() + this.numberGet();
+    let result: string = this.createCore(text);
+    return result;
+  }
+
+  public createAlphabet() {
+    // アルファベット
+    let text: string = this.lowerGet() + this.upperGet();
+    let result: string = this.createCore(text);
+    return result;
+  }
+
+  public createUpper() {
+    // 大文字
+    let text: string = this.upperGet();
+    let result: string = this.createCore(text);
+    return result;
+  }
+
+  public createLower() {
+    // 小文字
+    let text: string = this.lowerGet();
+    let result: string = this.createCore(text);
     return result;
   }
 
@@ -128,7 +173,15 @@ class PasswordCreater{
     if (notSymbol.length > 0) {
       // 記号が含まれていれば
       this.notSymbolArray = notSymbol.split("");
+    } else {
+      // 記号が含まれていなければ
+      this.notSymbolArray = [];
     }
+  }
+
+  public notSymbolClear() {
+    // 使わない記号をリセットする
+    this.notSymbolArray = [];
   }
 }
 
--- test/html/js/script.js
+++ test/html/js/script.js
@@ -1,6 +1,13 @@
 function create() {
   // パスワードを生成する(テスト)
   PasswordCreater.lengthChange(Number(document.getElementById("len").value));
+
+  // 使わない記号の設定
+  let notSymbol = "!<>?";
+  PasswordCreater.notSymbolChange(notSymbol);
+
+  PasswordCreater.test();
+
   let all = PasswordCreater.createAll();
   let normal = PasswordCreater.createNormal();
   let num = PasswordCreater.createNumber();
@@ -9,3 +16,5 @@ function create() {
   document.getElementById("result-normal").value = normal;
   document.getElementById("result-number").value = num;
 }
+
+PasswordCreater.test();
